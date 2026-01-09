827a1237775901ce4a59f6ac7ce9816610e79046
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-09 17:05:10 +0100

    installed necessary dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello Worlsd!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
