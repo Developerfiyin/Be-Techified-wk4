43dfa850069c982018ecbfc365092cb8df469e03
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-10 12:45:42 +0100

    Updated all routes

--- server.js
+++ server.js
@@ -7,11 +7,23 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
-app.use()
+app.use(cors())
+
+// Routes
+
+// Get all todos
 app.get('/todos', (req, res) => {
   res.send(200).json({message: "Hello World"})
 })
 
+// Create a new todo
+app.post('/todos', (req, res) => {
+  res.send(201).json({message: "Todo created"})
+})
+// Update a todo
+app.put('/todos/:id', (req, res) => {
+  res.send(200).json({message: "Todo updated"})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
